86b9a58a3b98a8fd47f3311a91eed611b90a9c1f
Spar <spar@Spars-MacBook-Air.local>
2019-03-22 10:34:40 -0500

    refactored SeasonDisplay component

--- src/SeasonDisplay.js
+++ src/SeasonDisplay.js
@@ -1,19 +1,36 @@
 import React from 'react';
 import { get } from 'http';
 
+const seasonConfig = {
+    summer: {
+        text: "Lets hit the beach!",
+        iconName: 'sun'
+    },
+    winter: {
+        text: "Burr, it is chilly!",
+        iconName: 'snowflake'
+    }
+};
+
 const getSeason = (lat, month) => {
     if (month > 2 && month < 9) {
         return lat > 0 ? 'summer' : 'winter';
     } else {
         return lat > 0 ? 'winter' : 'summer';
     }
-
-}
+};
 
 const SeasonDisplay = props => {
     const season = getSeason(props.lat, new Date().getMonth());
-    console.log(season);
-    return <div>Season Display</div>
+    const { text, iconName } = seasonConfig[season];
+
+    return (
+        <div>
+            <i className={`massive ${iconName} icon`} />
+            <h1>{text}</h1>
+            <i className={`massive ${iconName} icon`} />
+        </div >
+    );
 };
 
 export default SeasonDisplay;
