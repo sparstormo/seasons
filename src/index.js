799d9e46d3c92e6e3b116f8a65f91f55957de43d
Spar <spar@Spars-MacBook-Air.local>
2019-03-25 12:01:48 -0500

    refactor

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { compileFunction } from 'vm';
 import SeasonDisplay from './SeasonDisplay';
+import Spinner from './Spinner';
+
 
 class App extends React.Component {
 
@@ -17,19 +19,23 @@ class App extends React.Component {
         console.log('My component was just updated and rerendered.');
     }
 
-
-
-    //React says we have to define render!
-    render() {
+    renderContent() {
         if (this.state.errorMessage && !this.state.lat) {
             return <div>Error: {this.state.errorMessage}</div>;
         }
         if (!this.state.errorMessage && this.state.lat) {
             return <SeasonDisplay lat={this.state.lat} />;
         }
-        return <div>Loading...</div>;
+        return <Spinner message='Please accept location request' />;
+    }
 
-    };
+    render() {
+        return (
+            <div className="border red">
+                {this.renderContent()}
+            </div>
+        );
+    }
 }
 
 ReactDOM.render(<App />, document.querySelector('#root'));
